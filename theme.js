c12b4fdc18e90af9e3677e961e78c052327f71a8
kosajohnson <kosaj09@gmail.com>
2025-06-09 09:25:22 -0400

    Update theme.js
    Slowed down trailing sparkle effect

--- theme.js
+++ theme.js
@@ -14,5 +14,5 @@ document.addEventListener("mousemove", (e) => {
   trail.style.left = `${e.clientX}px`;
   trail.style.top = `${e.clientY}px`;
   document.body.appendChild(trail);
-  setTimeout(() => trail.remove(), 800);
+  setTimeout(() => trail.remove(), 2000);
 });
